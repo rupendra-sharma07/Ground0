784c1b49e95d0bd2df59cd9b58e84047bb5b1d7c
rupendras-optimus <rupendra.sharma@optimusinfo.com>
2013-06-22 19:05:05 +0530

    Ver 1.0.0.0

---
+++ ProcessForking/ProcessForking/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ProcessForking
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class ProcessStart
+    {
+        private readonly List<int> lstActiveProcessId = new List<int>();
+        private readonly List<int> lstInActiveProcessId = new List<int>();
+        private int counterActive;
+        private int counterCreate;
+        private int counterName;
+        private bool isActiveProcessExists;
+        private Process notePad;
+        private int totalprocess = 1000;
+        private int totalActiveProcess = 100;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            
+            var processStart = new ProcessStart();
+
+            processStart.fork();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void fork()
+        {
+            if (counterCreate < totalprocess)
+            {
+                if (counterActive < totalActiveProcess)
+                {
+                    var processStartInfo = new ProcessStartInfo("notepad.exe", "spawn");
+                    notePad = Process.GetCurrentProcess();
+                    notePad.StartInfo = processStartInfo;
+                    notePad.Start();
+
+                    // Add Process Id to Active process id List
+                    lstActiveProcessId.Add(notePad.Id);
+
+                    if (counterActive == 2)
+                    {
+                        //Print the Active Process Id
+                        foreach (int procId in lstActiveProcessId)
+                        {
+                            Console.WriteLine("Active Process Id =" + procId);
+                        }
+                    }
+                    counterActive++;
+                }
+                else
+                {
+                    var processStartInfo = new ProcessStartInfo("notepad.exe", "spawn");
+                    processStartInfo.CreateNoWindow = true;
+                    processStartInfo.UseShellExecute = false;
+                    processStartInfo.RedirectStandardInput = false;
+                    processStartInfo.RedirectStandardOutput = false;
+                    notePad = Process.GetCurrentProcess();
+                    notePad.StartInfo = processStartInfo;
+                    notePad.Start();
+                    //Console.WriteLine("Ideal ProcessId =" + notePad.Id);
+                    lstInActiveProcessId.Add(notePad.Id);
+                    notePad.WaitForExit();
+                }
+
+                counterCreate++;
+                notePad.EnableRaisingEvents = true;
+                // Add eevent handler on Process exit
+                notePad.Exited += OnProcessExisted;
+            }
+
+
+            if (counterCreate == 10)
+            {
+                Console.ReadLine();
+                return;
+            }
+            //Process Forking
+            fork();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnProcessExisted(object sender, EventArgs e)
+        {
+            if (sender != null)
+            {
+                var activeprocess = (Process) sender;
+                if (lstActiveProcessId.Contains(activeprocess.Id))
+                {
+                    //Print a Exit message for this process
+                    Console.WriteLine("Process " + activeprocess.Id + " has been completed its work and terminated");
+                    //Remove this process from the active list
+                    lstActiveProcessId.Remove(activeprocess.Id);
+
+                    //Activate the last waiting process
+                    if (lstInActiveProcessId.Count > 0)
+                    {
+                        Process inactiveProc = Process.GetProcessById(lstInActiveProcessId[0]);
+
+                        //Check if the process is resumed by operating System 
+                        if (inactiveProc.Responding)
+                        {
+                            //add Process Id in the active process list
+                            lstActiveProcessId.Add(inactiveProc.Id);
+
+                            //Print the new Active Process Ids
+                            foreach (int procId in lstActiveProcessId)
+                            {
+                                Console.WriteLine("Now the Active Process Id =" + procId);
+                            }
+                        }
+                        else
+                        {
+                            //Reduce active process number by 1
+                            counterActive--;
+                        }
+                    }
+                    else
+                    {
+                        //Reduce active process number by 1
+                        counterActive--;
+                    }
+                }
+                //Reduce total number of process by 1
+                counterCreate--;
+
+                fork();
+            }
+        }
+    }
+}
